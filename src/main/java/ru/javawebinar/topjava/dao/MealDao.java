8a60389f09cc65dfeb1dddf4c5c5a106aff4ffbc
Tyunikov <tyunikovag@gmail.com>
2021-02-08 20:31:01 +0300

    HW01 - add memory dao layer

---
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.dao;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+
+import java.util.List;
+
+public interface MealDao {
+    void addMeal(Meal meal);
+    void deleteMeal(Meal meal);
+    void deleteMeal(int id);
+    Meal getMealByID(int id);
+    void updateMeal(Meal meal);
+    List<Meal> getAllMeal();
+
+}
