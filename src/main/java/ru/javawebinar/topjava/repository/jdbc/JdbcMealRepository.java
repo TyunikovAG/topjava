3887790efe38f0952c769c6a17e6849a7fd252e3
Tyunikov <tyunikovag@gmail.com>
2021-02-24 02:24:30 +0300

    изменил запросы на "нормальные" со знаком вопроса. работает get, getAll and getBetweenHalfOpen

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,12 +10,15 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcMealRepository.class);
 
     private static final BeanPropertyRowMapper<Meal> ROW_MAPPER =
             BeanPropertyRowMapper.newInstance(Meal.class);
@@ -35,12 +40,21 @@ public class JdbcMealRepository implements MealRepository {
 
     @Override
     // null if updated meal does not belong to userId
+    // TODO: 24.02.2021 implemet
     public Meal save(Meal meal, int userId) {
+//        String query = String.format("SELECT * FROM meals " +
+//                "WHERE user_id = %d " +
+//                "AND id = %d " +
+//                "ORDER BY dateTime DESC", userId, id);
+//        log.debug(query);
+//        List<Meal> result = jdbcTemplate.query(query, ROW_MAPPER);
+//        return result.isEmpty()? null : result.get(0);
         return null;
     }
 
     @Override
     // false if meal does not belong to userId
+    // TODO: 24.02.2021 implemet
     public boolean delete(int id, int userId) {
         return false;
     }
@@ -48,19 +62,23 @@ public class JdbcMealRepository implements MealRepository {
     @Override
     // null if meal does not belong to userId
     public Meal get(int id, int userId) {
-        return null;
+        List<Meal> result = jdbcTemplate.query("SELECT * FROM meals " +
+                "WHERE user_id = ? AND id = ? " +
+                "ORDER BY dateTime DESC", ROW_MAPPER, userId, id);
+        return result.isEmpty()? null : result.get(0);
     }
 
     @Override
     // ORDERED dateTime desc
     public List<Meal> getAll(int userId) {
-        String query = String.format("SELECT * FROM meals WHERE user_id = %d ORDER BY dateTime", userId);
-        return jdbcTemplate.query(query, ROW_MAPPER);
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ? ORDER BY dateTime DESC", ROW_MAPPER, userId);
     }
 
     @Override
     // ORDERED dateTime desc
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals " +
+                "WHERE user_id = ? AND datetime >= ? AND datetime < ? " +
+                "ORDER BY dateTime DESC", ROW_MAPPER, userId, startDateTime, endDateTime);
     }
 }
